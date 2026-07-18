1be901603e73a419ce396e86b4c304340df58d83
Joao Gilberto Magalhaes <joao@byjg.com.br>
2026-07-18 15:16:05 -0400

    Add OA\Property to OaDeletedAt so deleted_at appears in the schema
    The OaDeletedAt trait only pulled in the DeletedAt field mapping but, unlike
    OaCreatedAt/OaUpdatedAt, never declared an #[OA\Property]. Models using it
    returned deleted_at in responses that were then rejected by contract validation
    ("property deletedAt has not defined in schema"). Mirror the other Oa* traits.

--- src/Trait/OaDeletedAt.php
+++ src/Trait/OaDeletedAt.php
@@ -2,9 +2,15 @@
 
 namespace ByJG\Gluo\Trait;
 
+use ByJG\MicroOrm\Attributes\FieldAttribute;
 use ByJG\MicroOrm\Trait\DeletedAt;
+use OpenApi\Attributes as OA;
 
 trait OaDeletedAt
 {
     use DeletedAt;
+
+    #[OA\Property(type: "string", format: "date-time", nullable: true)]
+    #[FieldAttribute(fieldName: "deleted_at", syncWithDb: false)]
+    protected string|null $deletedAt = null;
 }
